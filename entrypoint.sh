5d63730bfe891aecd82da5edca8b831555f51db0
machineglow <46592290+machineglow@users.noreply.github.com>
2025-08-11 12:38:05 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,15 @@
 CONFIG_FILE="$CONFIG_DIR/config.yaml"
 TEMPLATE_FILE="config-TEMPLATE.yaml"
 
-# Check if config.yaml exists; if not, copy from config-TEMPLATE.yaml
+# Check if template exists; if not, copy from config-TEMPLATE.yaml
 if [ ! -f "$CONFIG_DIR/$TEMPLATE_FILE" ]; then
     echo "config-TEMPLATE.yaml not found. Creating config-TEMPLATE.yaml..."
+    cp "$TEMPLATE_FILE" "$CONFIG_DIR/$TEMPLATE_FILE"
+fi
+
+# Check if config.yaml exists; if not, copy from config-TEMPLATE.yaml
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "config.yaml not found. Copying config-TEMPLATE.yaml..."
     cp "$TEMPLATE_FILE" "$CONFIG_DIR/$TEMPLATE_FILE"
 fi
 
